6d8023b52e4839ce84a602f3ddc79391fa9efebd
Mazhar Altincay <mazharaltincay89@gmail.com>
2025-03-24 00:41:39 +0100

    finished

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,7 +1,4 @@
 ﻿using Project;
 
 IDeviceManager manager = DeviceManagerFactory.Create("devices.txt");
-IDevice device = new EmbeddedDevices("1", "Embedeed", false, "123.123.123.123", "MD Ltd. home");
-
-manager.EditDevice("1", "EmbeddedDevices", "Embedded");
 manager.ShowAllDevices();
