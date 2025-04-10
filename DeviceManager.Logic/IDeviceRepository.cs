f4651b289d767d0ab31e0115cba1ff084781f865
Mazhar Altincay <mazharaltincay89@gmail.com>
2025-04-10 02:29:51 +0200

    cached successfully nad project structured aligned to requirments

--- DeviceManager.Logic/IDeviceRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using DeviceManager.Entities;
-
-namespace DeviceManager.Logic;
-
-public interface IDeviceRepository
-{
-    List<Device> LoadDevices();
-    void SaveDevices(List<Device> devices);
-}
