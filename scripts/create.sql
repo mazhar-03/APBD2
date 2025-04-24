3ccbcca4875eb055e59dbf9f86d445f0b9ec489f
Mazhar Altincay <mazharaltincay89@gmail.com>
2025-04-24 12:50:08 +0200

    Db renovated according to new requirements

--- scripts/create.sql
+++ scripts/create.sql
@@ -1,23 +1,31 @@
-﻿CREATE TABLE Smartwatches (
-                              Id NVARCHAR(5) PRIMARY KEY,
-                              Name NVARCHAR(30),
-                              IsOn BIT,
-                              BatteryPercentage INT
+﻿CREATE TABLE Device (
+                        Id NVARCHAR(5) PRIMARY KEY,
+                        Name NVARCHAR(30) NOT NULL,
+                        IsEnabled BIT NOT NULL
 );
 GO
 
-CREATE TABLE PersonalComputers (
-                                   Id NVARCHAR(5) PRIMARY KEY,
-                                   Name NVARCHAR(30),
-                                   IsOn BIT,
-                                   OperatingSystem NVARCHAR(30)
+CREATE TABLE Smartwatch (
+                            Id INT PRIMARY KEY,
+                            BatteryPercentage INT NOT NULL,
+                            DeviceId NVARCHAR(5) NOT NULL,
+                            FOREIGN KEY (DeviceId) REFERENCES Device(Id)
 );
 GO
 
-CREATE TABLE EmbeddedDevices (
-                                 Id NVARCHAR(5) PRIMARY KEY,
-                                 Name NVARCHAR(30),
-                                 IsOn BIT,
-                                 IpName NVARCHAR(20),
-                                 NetworkName NVARCHAR(40)
+CREATE TABLE PersonalComputer (
+                                  Id INT PRIMARY KEY,
+                                  OperationSystem NVARCHAR(20),
+                                  DeviceId NVARCHAR(5) NOT NULL,
+                                  FOREIGN KEY (DeviceId) REFERENCES Device(Id)
 );
+GO
+
+CREATE TABLE Embedded (
+                          Id INT PRIMARY KEY,
+                          IpAddress NVARCHAR(20) NOT NULL,
+                          NetworkName NVARCHAR(30) NOT NULL,
+                          DeviceId NVARCHAR(5) NOT NULL,
+                          FOREIGN KEY (DeviceId) REFERENCES Device(Id)
+);
+GO
--- scripts/insert.sql
+++ scripts/insert.sql
@@ -1,17 +1,29 @@
-﻿INSERT INTO Smartwatches (Id, Name, IsOn, BatteryPercentage) VALUES
-    ('SW-01', 'Apple Watch', 1, 75),
-    ('SW-02', 'Xiaomi Smartwatch', 0, 38),
-    ('SW-03', 'Garmin Forerunner 55', 1, 98);
-
+﻿INSERT INTO Device (Id, Name, IsEnabled) VALUES
+                                             ('SW-01', 'Apple Watch', 1),
+                                             ('SW-02', 'Xiaomi Smartwatch', 0),
+                                             ('SW-03', 'Garmin Forerunner 55', 1),
+                                             ('P-01', 'Lenovo Legion 5', 0),
+                                             ('P-02', 'Dell QAZ', 1),
+                                             ('P-03', 'MacBook Air', 1),
+                                             ('ED-01', 'Sensor Unit', 1),
+                                             ('ED-02', 'Extended Microphone Sensor', 1),
+                                             ('ED-03', 'Audio Device', 1);
 GO
 
-INSERT INTO PersonalComputers (Id, Name, IsOn, OperatingSystem) VALUES
-    ('P-01', 'Lenovo Legion 5', 0, 'Windows 10'),
-    ('P-02', 'Dell QAZ', 1, 'Linux'),
-    ('P-03', 'MacBook Air', 1, 'MacOS');
-
+INSERT INTO Smartwatch (Id, BatteryPercentage, DeviceId) VALUES
+                                                             (01, 75, 'SW-01'),
+                                                             (02, 38, 'SW-02'),
+                                                             (03, 98, 'SW-03');
 GO
-INSERT INTO EmbeddedDevices (Id, Name, IsOn, IpName, NetworkName) VALUES
-    ('ED-01', 'Sensor Unit', 1, '192.168.1.10', 'MD Ltd. Home Network'),
-    ('ED-02', 'Extended Microphone Sensor', 1, '192.251.1.230', 'MD Ltd. Office Network'),
-    ('ED-03', 'Audio Device', 1, '62.62.0.0', 'Open Network');
+
+INSERT INTO PersonalComputer (Id, OperationSystem, DeviceId) VALUES
+                                                                 (01, 'Windows 10', 'P-01'),
+                                                                 (02, 'Linux', 'P-02'),
+                                                                 (03, 'MacOS', 'P-03');
+GO
+
+INSERT INTO Embedded (Id, IpAddress, NetworkName, DeviceId) VALUES
+                                                                (01, '192.168.1.10', 'MD Ltd. Home Network', 'ED-01'),
+                                                                (02, '192.251.1.230', 'MD Ltd. Office Network', 'ED-02'),
+                                                                (03, '62.62.0.0', 'Open Network', 'ED-03');
+GO
