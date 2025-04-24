8070f2653ba7ad84f6e060fd2842cf3bde7ded6a
Mazhar Altincay <mazharaltincay89@gmail.com>
2025-04-24 16:34:55 +0200

    Allocated more space for ID

--- scripts/create.sql
+++ scripts/create.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE Device (
-                        Id NVARCHAR(5) PRIMARY KEY,
+                        Id NVARCHAR(10) PRIMARY KEY,
                         Name NVARCHAR(30) NOT NULL,
                         IsEnabled BIT NOT NULL
 );
@@ -8,15 +8,15 @@ GO
 CREATE TABLE Smartwatch (
                             Id INT PRIMARY KEY,
                             BatteryPercentage INT NOT NULL,
-                            DeviceId NVARCHAR(5) NOT NULL,
+                            DeviceId NVARCHAR(10) NOT NULL,
                             FOREIGN KEY (DeviceId) REFERENCES Device(Id)
 );
 GO
 
 CREATE TABLE PersonalComputer (
                                   Id INT PRIMARY KEY,
-                                  OperationSystem NVARCHAR(20),
-                                  DeviceId NVARCHAR(5) NOT NULL,
+                                  OperationSystem NVARCHAR(20) NULL,
+                                  DeviceId NVARCHAR(10) NOT NULL,
                                   FOREIGN KEY (DeviceId) REFERENCES Device(Id)
 );
 GO
@@ -25,7 +25,7 @@ CREATE TABLE Embedded (
                           Id INT PRIMARY KEY,
                           IpAddress NVARCHAR(20) NOT NULL,
                           NetworkName NVARCHAR(30) NOT NULL,
-                          DeviceId NVARCHAR(5) NOT NULL,
+                          DeviceId NVARCHAR(10) NOT NULL,
                           FOREIGN KEY (DeviceId) REFERENCES Device(Id)
 );
 GO
